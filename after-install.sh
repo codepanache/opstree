820f69d1ba62b25576be340f3c234d1c5bff8429
Play9win <rohit.kumar@sourcefuse.com>
2020-10-11 17:39:53 +0530

    version 3.0

--- after-install.sh
+++ after-install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo service nginx start
+sudo pm2 restart /var/www/html/app.js
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/*', function(req, res){
-   res.send("Welcome V2.0");
+   res.send("Welcome V3.0");
 });
 app.listen(process.env.PORT || 3000);
